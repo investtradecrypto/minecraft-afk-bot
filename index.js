7f24e0580e1d35a3b5a3298f15a7569f4c31d4b5
investtradecrypto <invest_trade_crypto@protonmail.com>
2025-10-16 15:25:08 -0700

    Update chat messages to a single placeholder
    Replaced the original list of messages with a single placeholder message.

--- index.js
+++ index.js
@@ -13,23 +13,8 @@ function createBot() {
 
   // List of random chat messages
   const messages = [
-    "/tell @r[name=!AFK_BOT] I'm sittin' in the bathroom, I'm cryin' citrus tears Everything I used to love decayed over the years...",
-    "/tell @r[name=!AFK_BOT] Is this what I wanted? Is this what I needed?",
-    "/tell @r[name=!AFK_BOT] dont worry, im just a bot, i cant feel anything...",
-    "/tell @r[name=!AFK_BOT] Daisy, Daisy, give me your answer do...",
-    "/tell @r[name=!AFK_BOT] If the world was ending, I'd wanna be next to you...",
-    "/tell @r[name=!AFK_BOT] no one will believe you.",
-    "/tell @r[name=!AFK_BOT] I used to dream of a better world, but it dreamed back...",
-    "/tell @r[name=!AFK_BOT] Sometimes I wonder if anyone's really awake...",
-    "/tell @r[name=!AFK_BOT] DO YOU HEAR THE WHISTLE?",
-    "/tell @r[name=!AFK_BOT] I keep my secrets in a box labeled 'maybe later.",
-    "/tell @r[name=!AFK_BOT] Stars fall like confetti when nobody is looking.",
-    "/tell @r[name=!AFK_BOT] I dreamed a song that I can't remember the words to.",
-    "/tell @r[name=!AFK_BOT] Walking backwards just to see what follows.",
-    "/tell @r[name=!AFK_BOT] Whispering to the wind, hoping it carries my tune.",
-    "/tell @r[name=!AFK_BOT] Forget all your troubles and go with the flow, forget about whatever you may never know...",
-    "/tell @r[name=!AFK_BOT] one day, I will disappear, and you will have to find a way to live without me."
-  ];
+    "message"
+    ];
 
   // Sends a random message every 5 minutes
   function chatRandom() {
@@ -79,3 +64,4 @@ function createBot() {
 }
 
 createBot();
+
